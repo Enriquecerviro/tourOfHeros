a0027d1bc9595dcaa183d1e4d8efc002b1c92bd7
Enrique Cervino <enrique.cervirodam@gmail.com>
2019-09-19 13:15:48 +0200

    HeroDetails

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Hero } from '../hero'
+import { HEROES } from '../mock-heroes';
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
@@ -7,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeroesComponent implements OnInit {
 
+  /**
+   * AQUI LO QUE HACEMOS ES EXPONER EL ARRAY
+   * PARA ATARLO EN UNA VAR.
+   */
+  heroes = HEROES;
+  selectedHero: Hero;//selectedHero TENDRA UN ID Y UN NOMBRE
+
   constructor() { }
 
   ngOnInit() {
+  }
+  onSelect(hero: Hero): void{
+    this.selectedHero = hero;
   }
 
 }
